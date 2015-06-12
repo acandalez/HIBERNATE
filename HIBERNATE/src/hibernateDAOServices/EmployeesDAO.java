cbe22bdd950722dd0963156e97e68b796be2e95d
acandalez <acandalez@gmail.com>
2015-06-12 19:00:10 +0200

    Hibernate

---
+++ HIBERNATE/src/hibernateDAOServices/EmployeesDAO.java
@@ -0,0 +1,37 @@
+package hibernateDAOServices;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import clases_y_mapas_codegeneration.Employees;
+
+public class EmployeesDAO {
+
+	Session sesion = null;
+
+	public EmployeesDAO() {
+
+	}
+
+	public Session getSesion() {
+		return sesion;
+	}
+
+	public void setSesion(Session sesion) {// Empleado DAO necesita una sesion
+											// con lo que con esto le damos una
+											// sesion asignada en servicios
+		this.sesion = sesion;
+	}
+
+	public List<Employees> obtenerEmpleado() {
+		List<Employees> ls = null;
+
+		ls = this.getSesion().createSQLQuery("select * from employees")
+				.addEntity(Employees.class).list();
+
+		return ls;
+
+	}
+
+}
